220f0b1bb1aea3b4bff9e0b65551f6b012d3c2c2
Remo Schmutz <remoschmutz@bluewin.ch>
2024-08-30 16:22:29 +0200

    Update getPostText.ts

--- src/lib/getPostText.ts
+++ src/lib/getPostText.ts
@@ -11,6 +11,7 @@ const POSTED_PAPERS_PATH = './postedPapers.json';
 const postedPapers = JSON.parse(fs.readFileSync(POSTED_PAPERS_PATH, 'utf8'));
 
 const ONE_DAY = 60 * 60 * 1000;  // One hour in milliseconds
+const MAX_POSTS_PER_RUN = 20;    // Maximum number of papers to post in one run
 
 export default async function getPostText() {
   const parser = new Parser();
@@ -22,15 +23,20 @@ export default async function getPostText() {
     const currentDate = new Date();
 
     const isAlreadyPosted = postedPapers.papers.some((paper: Paper) => paper.title === item.title && paper.link === item.link);
-const formattedText = `${item.title}: ${item.link}`;
+    const formattedText = `${item.title}: ${item.link}`;
     const isWithinLengthLimit = formattedText.length <= 290;
 
     if (!isAlreadyPosted && (currentDate.getTime() - publicationDate.getTime() <= ONE_DAY) && isWithinLengthLimit) {
       papersToPost.push({
         title: item.title,
         link: item.link,
-        formattedText: `${item.title}: ${item.link}`
+        formattedText: formattedText
       });
+
+      // Stop if we have reached the maximum number of posts for this run
+      if (papersToPost.length >= MAX_POSTS_PER_RUN) {
+        break;
+      }
     }
   }
 
